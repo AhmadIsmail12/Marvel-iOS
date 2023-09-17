b8a9a325fe75d783ac0454cec50c64ee3c58d54b
AhmadIsmail12 <ahmad.b.ismail1@gmail.com>
2023-09-17 11:14:05 +0300

    Initial Commit

---
+++ Mavel-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mavel-iOS
+//
+//  Created by Ahmad Ismail on 17/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
