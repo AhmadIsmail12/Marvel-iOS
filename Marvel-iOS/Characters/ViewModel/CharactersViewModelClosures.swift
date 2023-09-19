7805f1a768014718ee99a210e16dc532fddae2e4
AhmadIsmail12 <ahmad.b.ismail1@gmail.com>
2023-09-19 09:07:10 +0300

    Create Characters View Model and Closures where all the logic is there, getting data and handle navigation when selecting a character a closure is call and will navigate through coordinator

---
+++ Marvel-iOS/Characters/ViewModel/CharactersViewModelClosures.swift
@@ -0,0 +1,12 @@
+//
+//  CharactersViewModelClosures.swift
+//  Mavel-iOS
+//
+//  Created by Ahmad Ismail on 17/09/2023.
+//
+
+import Foundation
+
+public struct CharactersViewModelClosures {
+    let showCharacterDetailsViewController: (Character) -> Void
+}
