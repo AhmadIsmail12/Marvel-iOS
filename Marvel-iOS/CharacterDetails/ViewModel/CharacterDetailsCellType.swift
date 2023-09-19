c0948eb5846f018834fe8bebdfd80ef8c71ebd47
AhmadIsmail12 <ahmad.b.ismail1@gmail.com>
2023-09-19 09:10:53 +0300

    Creating and Enum For Character Detail Cell Types

---
+++ Marvel-iOS/CharacterDetails/ViewModel/CharacterDetailsCellType.swift
@@ -0,0 +1,13 @@
+//
+//  CharacterDetailsCellType.swift
+//  Marvel-iOS
+//
+//  Created by Ahmad Ismail on 17/09/2023.
+//
+
+import Foundation
+
+enum CharacterDetailsCellType {
+    case header
+    case content(String, [CharacterContent])
+}
